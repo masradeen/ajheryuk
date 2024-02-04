22b738fe778b4db9e404bab0a7a49d862b5c5f00
MasRadeen <sigitnugrohop@gmail.com>
2024-02-04 21:21:55 +0800

    Membuat file welcome page dan menambahkan icon logo

---
+++ lib/pages/welcome_page.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class WelcomePage extends StatelessWidget {
+
+  static const nameRoute = '/welcomepage';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width * 0.6,
+              height: MediaQuery.of(context).size.width * 0.6,
+              child: Image.asset(
+                "assets/logo/logo.png",
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(height: 100,),
+            Text(
+              "Welcome to Ajheryuk",
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10,),
+            Text(
+              "Best and popular apps for live education course from home",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 16,
+                color: Color(0xFF8C8C8C),
+              ),
+            ),
+            SizedBox(height: 75,),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text(
+                  "Get Started",
+                ),
+                style: ElevatedButton.styleFrom(
+                  primary: Color(0xFFEC5F5F),
+                  padding: EdgeInsets.symmetric(vertical: 20),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
